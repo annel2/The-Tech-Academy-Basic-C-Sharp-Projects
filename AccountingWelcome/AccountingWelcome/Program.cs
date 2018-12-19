c38812ae0faef079eff6e5cbf1ce61f3fc76c73e
Anne <saigonmafia228@gmail.com>
2018-12-18 19:23:49 -0800

    good to go

--- AccountingWelcome/AccountingWelcome/Program.cs
+++ AccountingWelcome/AccountingWelcome/Program.cs
@@ -6,7 +6,10 @@ namespace AccountingWelcome
     class Program
     {
         static void Main()
-        {Console.WriteLine
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("We are 'accouting'on you!");
+            Console.ReadLine();
         }
     }
 }
